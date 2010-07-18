18e9bf21321f2367a41be7a878bbf90f88699839
Tim Kersten <tim@io41.com>
2010-07-18 20:06:48 +0100

    Add utility method for getting the total collision count.

--- pieces.py
+++ pieces.py
@@ -1,6 +1,7 @@
-#0000000000000000 0000000000000000 0000000000000000 0000000000000000
+#0000-0000-0000-0000 0000-0000-0000-0000 0000-0000-0000-0000 0000-0000-000-00000
 
 #0000 0000 0000 0000
+
 
 raw_pieces = [
     'x...xx...xx.....',
@@ -47,3 +48,21 @@ def make_list(pieces):
 
 pieces = make_binary(raw_pieces)
 list_pieces = make_list(pieces)
+
+collision = lambda x, y: x & y
+
+def total_collision_count(pieces):
+    """
+    Loop over pieces and return a total collision count.
+    """
+    cube = 0
+    collision_count = 0
+    for i, piece in enumerate(pieces):
+        c = bin(collision(cube, piece)).count('1')
+        if c:
+            print "%d has %d collisions" % (i+1, c)
+            collision_count += c
+        cube |= piece
+    return collision_count
+
+print "Total collisions: %d" % total_collision_count(pieces)
