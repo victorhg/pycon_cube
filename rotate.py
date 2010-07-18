203dca903f7367f61e698e286afdc8a75165bcf9
Maciej Bliziński <maciej.blizinski@gmail.com>
2010-07-18 21:46:48 +0100

    Moved the functionality into a class.

--- rotate.py
+++ rotate.py
@@ -7,60 +7,62 @@ import itertools
 import pprint
 import math
 
-def mappings(size):
-  """Calculates mappings between coordinates and indexes."""
-  dim_by_idx = {}
-  idx_by_dim = {}
-  count = itertools.count()
-  for z in range(size):
-    for y in range(size):
-      for x in range(size):
-        idx = count.next()
-        dim = (x, y, z)
-        idx_by_dim[dim] = idx
-        dim_by_idx[idx] = dim
-  return idx_by_dim, dim_by_idx
+class Rotator(object):
+  
+  OFFSET = 1.5
 
+  def __init__(self, mapping_size=4):
+    self.idx_by_dim, self.dim_by_idx = self.Mappings(mapping_size)
 
-def rotate_dim_x(x, y, z):
-  # p = numpy.array([x, y, z])
-  # x' = x cos f - y sin f
-  # y' = y cos f + x sin f
-  # The dimensions at play are: y, z
-  offset = 1.5
-  my_cos = 0
-  my_sin = 1
-  x -= offset
-  y -= offset
-  z -= offset
-  new_x = x
-  new_y = y * my_cos - z * my_sin
-  new_z = z * my_cos + y * my_sin
-  new_x += offset
-  new_y += offset
-  new_z += offset
-  return (int(new_x), int(new_y), int(new_z))
+  def Mappings(self, size):
+    """Calculates mappings between coordinates and indexes."""
+    dim_by_idx = {}
+    idx_by_dim = {}
+    count = itertools.count()
+    for z in range(size):
+      for y in range(size):
+        for x in range(size):
+          idx = count.next()
+          dim = (x, y, z)
+          idx_by_dim[dim] = idx
+          dim_by_idx[idx] = dim
+    return idx_by_dim, dim_by_idx
 
+  def RotateCoordsX(self, x, y, z):
+    # p = numpy.array([x, y, z])
+    # x' = x cos f - y sin f
+    # y' = y cos f + x sin f
+    # The dimensions at play are: y, z
+    my_cos = 0
+    my_sin = 1
+    x -= self.OFFSET
+    y -= self.OFFSET
+    z -= self.OFFSET
+    new_x = x
+    new_y = y * my_cos - z * my_sin
+    new_z = z * my_cos + y * my_sin
+    new_x += self.OFFSET
+    new_y += self.OFFSET
+    new_z += self.OFFSET
+    return (int(new_x), int(new_y), int(new_z))
 
-def x_rotation_mapping():
-  """Rotates along the X axis, top side forward."""
-  idx_by_dim, dim_by_idx = mappings(4)
-  x_rot = {}
-  for dim in idx_by_dim:
-    old_idx = idx_by_dim[dim]
-    new_dim = rotate_dim_x(*dim)
-    new_idx = idx_by_dim[new_dim]
-    x_rot[old_idx] = new_idx
-  return x_rot
+  def GetRotationMappingX(self):
+    """Rotates along the X axis, top side forward."""
+    x_rot = {}
+    for dim in self.idx_by_dim:
+      old_idx = self.idx_by_dim[dim]
+      new_dim = self.RotateCoordsX(*dim)
+      new_idx = self.idx_by_dim[new_dim]
+      x_rot[old_idx] = new_idx
+    return x_rot
 
-
-def rotate_x(n):
-  assert len(n) == 64, "n has the wrong length."
-  mapping = x_rotation_mapping()
-  new = range(64)
-  for i in range(64):
-    new[mapping[i]] = n[i]
-  return new
+  def RotateElementX(self, n):
+    assert len(n) == 64, "n has the wrong length."
+    mapping = self.GetRotationMappingX()
+    new = range(64)
+    for i in range(64):
+      new[mapping[i]] = n[i]
+    return new
 
 
 class FooTest(unittest.TestCase):
@@ -90,19 +92,21 @@ class FooTest(unittest.TestCase):
                  0, 0, 0, 0, # 52
                  0, 0, 0, 0, # 56
                  0, 0, 0, 0,] # 60
-  # top layer of the cube
+    # top layer of the cube
+    self.r = Rotator()
+    self.r2 = Rotator(2)
 
   def test_1(self):
     expected = copy.copy(self.data)
     expected[12] = 1
     self.data[0] = 1
-    self.assertEqual(expected, rotate_x(self.data))
+    self.assertEqual(expected, self.r.RotateElementX(self.data))
 
   def test_2(self):
     expected = copy.copy(self.data)
     expected[24] = 1
     self.data[20] = 1
-    self.assertEqual(expected, rotate_x(self.data))
+    self.assertEqual(expected, self.r.RotateElementX(self.data))
 
   def testX_3(self):
     data = range(64)
@@ -126,7 +130,7 @@ class FooTest(unittest.TestCase):
                 44, 45, 46, 47,
                 28, 29, 30, 31,
                 12, 13, 14, 15]
-    self.assertEqual(expected, rotate_x(data))
+    self.assertEqual(expected, self.r.RotateElementX(data))
 
   def testX_4(self):
     data = (list('a' * 16)
@@ -152,7 +156,7 @@ class FooTest(unittest.TestCase):
                 'c', 'c', 'c', 'c',
                 'b', 'b', 'b', 'b',
                 'a', 'a', 'a', 'a']
-    self.assertEqual(expected, rotate_x(data))
+    self.assertEqual(expected, self.r.RotateElementX(data))
 
   def disabled_testY_1(self):
     expected = copy.copy(self.data)
@@ -169,7 +173,6 @@ class FooTest(unittest.TestCase):
     self.assertEqual(expected, rotate_y(data))
 
   def test_mappings_1(self):
-    idx_by_dim, dim_by_idx = mappings(2)
     expected = {
         (0, 0, 0): 0,
         (1, 0, 0): 1,
@@ -180,10 +183,9 @@ class FooTest(unittest.TestCase):
         (0, 1, 1): 6,
         (1, 1, 1): 7,
     }
-    self.assertEqual(expected, idx_by_dim)
+    self.assertEqual(expected, self.r2.idx_by_dim)
 
   def test_mappings_2(self):
-    idx_by_dim, dim_by_idx = mappings(2)
     expected = {
         0: (0, 0, 0),
         1: (1, 0, 0),
@@ -194,7 +196,7 @@ class FooTest(unittest.TestCase):
         6: (0, 1, 1),
         7: (1, 1, 1)
     }
-    self.assertEqual(expected, dim_by_idx)
+    self.assertEqual(expected, self.r2.dim_by_idx)
 
 if __name__ == '__main__':
   unittest.main()
